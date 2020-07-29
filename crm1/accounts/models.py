5afc1cf87676db10bea2928cbe5b0fcc71ce74db
Sébastien <s3bc40@pop-os.localdomain>
2020-07-29 22:19:11 +0200

    Intro to signals with profile example post_save WIP on request signals and accounts app

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
@@ -47,3 +48,13 @@ class Order(models.Model):
     
     def __str__(self):
         return self.product.name
+
+class Profile(models.Model):
+    user = models.OneToOneField(user, on_delete=models.CASCADE, blank=True, null=True)
+    first_name = models.CharField(max_length=200, blank=True, null=True)
+    last_name = models.CharField(max_length=200, blank=True, null=True)
+    phone = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.user)
+        
--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # import applications here
-    'accounts',
+    'accounts.apps.AccountsConfig',
     'django_filters',
 ]
 
